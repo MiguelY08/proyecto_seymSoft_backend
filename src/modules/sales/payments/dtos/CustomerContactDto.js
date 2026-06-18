fd3dd1f21b8d014f192874e8243c203c84ae8b35
MiguelY08 <yepesangel8@gmail.com>
2026-06-17 19:05:46 -0500

    cambios en pagos y abonos

--- src/modules/sales/payments/dtos/CustomerContactDto.js
+++ src/modules/sales/payments/dtos/CustomerContactDto.js
@@ -1,10 +1,21 @@
 import serializeBigInt from "../helpers/serializeBigInt.js";
 
 export default class CustomerContactDto {
-  constructor({ idClient, fullName, phone, overdueCredits }) {
+  constructor({
+    idClient,
+    fullName,
+    phone,
+    lastPaymentDate,
+    overdueCredits,
+  }) {
     this.idClient = idClient;
+
     this.fullName = fullName;
+
     this.phone = serializeBigInt(phone);
+
+    this.lastPaymentDate = lastPaymentDate;
+
     this.overdueCredits = overdueCredits;
   }
 }
